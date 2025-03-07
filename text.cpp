2f45bdc8ddb4ae3b9cac5c8d2cc1e77770ac2a95
Kunal8931 <kunalkk14014@gmail.com>
2025-03-07 11:34:30 +0530

    uploaded kunal.cpp code in it

---
+++ text.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int kunal(int a,int b){
+ if(b==0) return a;
+ else return kunal(b,a%b);
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<1<<" "<<n-1<<endl;
+    }
+}
